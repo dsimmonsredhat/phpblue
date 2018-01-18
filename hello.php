31503e8912d57e37b6bf4fc827eceb2df2d2cf20
dsimmonsredhat <dsimmons@redhat.com>
2018-01-18 14:56:30 -0500

    qwe
    aweqe

--- hello.php
+++ hello.php
@@ -5,7 +5,6 @@
  <body>
  <?php echo '<p>Hello I am Feeling</p>'; ?> 
  <?php echo "<font color='blue'>Blue</font>"; ?>
-<?php echo '<p>Applying a Patch</p>'; ?> 
  <?php header("Refresh:0"); ?>
  </body>
 </html>
